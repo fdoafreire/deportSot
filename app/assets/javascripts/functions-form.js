b72c60563d8478cb0bc89c96bc165ef0d8da19f7
Fernando Freire Gomez <fdoafreire@gmail.com>
2016-10-14 09:18:58 -0500

    ajuste formulario programación de partidos

--- app/assets/javascripts/functions-form.js
+++ app/assets/javascripts/functions-form.js
@@ -1,3 +1,3 @@
 	$(document).ready(function(){
-	$('#championship_date_initial_inscription , #championship_date_end_inscription').datepicker({ dateFormat: "yy-mm-dd" });
+	$('#championship_date_initial_inscription , #championship_date_end_inscription, #date_start').datepicker({ dateFormat: "yy-mm-dd" });
 });
