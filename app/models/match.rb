b522c5591cf6d2527ec6d8d4a1f7c4dd436694db
Fernando freire <fdoafreire@gmail.com>
2016-10-19 21:11:28 -0500

    se adiciona la relación entre los modelos que intervienen en la tabla detalle de partido

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,4 +2,5 @@ class Match < ApplicationRecord
   belongs_to :local, :class_name => "Team",:foreign_key  => "local_id" 
   belongs_to :visitant, :class_name => "Team",:foreign_key  => "visitant_id" 
   belongs_to :championship
+  has_many :details_matches
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,5 @@ class Team < ApplicationRecord
   has_many :locals, :class_name => "Match", :foreign_key  => "local_id"
   has_many :visitants, :class_name => "Match", :foreign_key  => "visitant_id"
   belongs_to :championship
+  has_many :details_matches
 end
