fdc628a2dc0f42aca4699d6b8ecefa870260dc37
Fernando freire <fdoafreire@gmail.com>
2016-11-21 23:04:23 -0500

    se adiciona nuevo estilo login

--- app/assets/javascripts/functions-form.js
+++ app/assets/javascripts/functions-form.js
@@ -26,4 +26,24 @@ $(document).ready(function(){
 	});
 
 
+    $('.show-register-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-login-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.login-form').fadeOut('fast', function(){
+    			$('.register-form').fadeIn('fast');
+    		});
+    	}
+    });
+    // ---
+    $('.show-login-form').on('click', function(){
+    	if( ! $(this).hasClass('active') ) {
+    		$('.show-register-form').removeClass('active');
+    		$(this).addClass('active');
+    		$('.register-form').fadeOut('fast', function(){
+    			$('.login-form').fadeIn('fast');
+    		});
+    	}
+    });
+
 });
